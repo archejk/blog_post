2de0134b623af173b08a1250b08be948f8aeb01e
archejk <jkarche13@gmail.com>
2025-06-05 00:20:26 +0800

    [Test] Create Rspec for requests

--- spec/requests/blog_posts_spec.rb
+++ spec/requests/blog_posts_spec.rb
@@ -1,53 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe "BlogPosts", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+  let(:blog_post) { FactoryBot.create(:blog_post, author: user) }
+
+  before do
+    sign_in user
+  end
+
   describe "GET /index" do
     it "returns http success" do
-      get "/blog_posts/index"
+      get blog_posts_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/blog_posts/show"
+      get blog_post_path(blog_post)
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /new" do
     it "returns http success" do
-      get "/blog_posts/new"
+      get new_blog_post_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/blog_posts/create"
-      expect(response).to have_http_status(:success)
+  describe "POST /create" do
+    it "creates a blog post and returns redirect" do
+      post blog_posts_path, params: { blog_post: { title: "New Post", content: "Some content" } }
+      expect(response).to have_http_status(:redirect) # usually redirects after create
+      follow_redirect!
+      expect(response.body).to include("New Post")
     end
   end
 
   describe "GET /edit" do
     it "returns http success" do
-      get "/blog_posts/edit"
+      get edit_blog_post_path(blog_post)
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/blog_posts/update"
-      expect(response).to have_http_status(:success)
+  describe "PATCH /update" do
+    it "updates the blog post and returns redirect" do
+      patch blog_post_path(blog_post), params: { blog_post: { title: "Updated Title" } }
+      expect(response).to have_http_status(:redirect)
+      follow_redirect!
+      expect(response.body).to include("Updated Title")
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/blog_posts/destroy"
-      expect(response).to have_http_status(:success)
+  describe "DELETE /destroy" do
+    it "deletes the blog post and redirects" do
+      delete blog_post_path(blog_post)
+      expect(response).to have_http_status(:redirect)
+      follow_redirect!
+      expect(response.body).not_to include(blog_post.title)
     end
   end
-
 end
--- spec/requests/feedbacks_spec.rb
+++ spec/requests/feedbacks_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Feedbacks", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/feedbacks/create"
-      expect(response).to have_http_status(:success)
-    end
+  let!(:user) { User.create!(email: "taylor@example.com", password: "password", name: "Taylor Swift") }
+  let!(:blog_post) { BlogPost.create!(title: "All of the Girls You Loved Before", content: "All of the Girls You Loved Before, But I love you more", author: user) }
+
+  before do
+    sign_in user
   end
 
+  describe "POST /blog_posts/:blog_post_id/feedbacks" do
+    it "creates feedback and redirects" do
+      post "/blog_posts/#{blog_post.id}/feedbacks", params: {
+        feedback: { message: "Slayyy, niceee!!! :)" }
+      }
+
+      expect(response).to have_http_status(:redirect)
+      follow_redirect!
+      expect(response.body).to include("Slayyy, niceee!!! :)")
+    end
+  end
 end
